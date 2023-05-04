11d16640d369abc02d25cbbe80bed83e5b44a81f
Helen Jones <helenfrancesjones@gmail.com>
2023-05-03 19:11:52 -0500

    tests not all passing yet

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -3,7 +3,9 @@ package org.launchcode.techjobs.oo.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.launchcode.techjobs.oo.Job;
+import org.launchcode.techjobs.oo.*;
+
+import javax.naming.Name;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -20,4 +22,24 @@ public class JobTest {
         assertNotEquals(job1.getId(), job2.getId());
     }
 
+    @Test
+    public void testToStringStartsAndEndsWithNewLine() {
+        Job job1 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
+        assertEquals('\n', job1.toString().charAt(0));
+        assertEquals('\n', job1.toString().charAt(job1.toString().length() - 1));
+    }
+
+    @Test
+    public void testToStringContainsCorrectLabelsAndData() {
+        Job job1 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
+        assertEquals("\nID: " + job1.getId() + "\nName: \nEmployer: \nLocation: \nPosition Type: \nCore Competency: \n", job1.toString());
+
+    }
+
+    @Test
+    public void testToStringHandlesEmptyField() {
+        Job job1 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
+        assertEquals("\nID: " + job1.getId() + "\nName: \nEmployer: \nLocation: \nPosition Type: \nCore Competency: \n", job1.toString());
+
+    }
 }
