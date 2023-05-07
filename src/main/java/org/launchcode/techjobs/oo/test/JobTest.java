8bbbdf0525699e75d46ba88280f3bf6cc750bd52
Helen Jones <helenfrancesjones@gmail.com>
2023-05-06 20:29:12 -0500

    location returning null of string tests

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -5,9 +5,8 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.launchcode.techjobs.oo.*;
 
-import javax.naming.Name;
-
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertNotEquals;
 
 /**
@@ -21,25 +20,42 @@ public class JobTest {
         Job job2 = new Job();
         assertNotEquals(job1.getId(), job2.getId());
     }
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
 
+    assertTrue(job3.getName() instanceof String);
+    assertTrue(job3.getEmployer() instanceof Employer);
+    assertTrue(job3.getLocation() instanceof Location);
+    assertTrue(job3.getPositionType() instanceof PositionType);
+    assertTrue(job3.getCoreCompetency() instanceof CoreCompetency);
+
+
+
+    assertEquals("Product tester",job3.getName());
+    assertEquals("ACME",job3.getEmployer().getValue());
+    assertEquals("Desert",job3.getLocation().getValue());
+    assertEquals("Quality control",job3.getPositionType().getValue());
+    assertEquals("Persistence",job3.getCoreCompetency().getValue());
+}
     @Test
     public void testToStringStartsAndEndsWithNewLine() {
-        Job job1 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
+        Job job1 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
         assertEquals('\n', job1.toString().charAt(0));
         assertEquals('\n', job1.toString().charAt(job1.toString().length() - 1));
     }
 
     @Test
     public void testToStringContainsCorrectLabelsAndData() {
-        Job job1 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
-        assertEquals("\nID: " + job1.getId() + "\nName: \nEmployer: \nLocation: \nPosition Type: \nCore Competency: \n", job1.toString());
+        Job job3 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        assertEquals("\nID: " + job3.getId() + "\nName: Product Tester\nEmployer: ACME \nLocation: Desert\nPosition Type: Quality control\nCore Competency: Persistence\n", job3.toString());
 
     }
 
     @Test
     public void testToStringHandlesEmptyField() {
-        Job job1 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
-        assertEquals("\nID: " + job1.getId() + "\nName: \nEmployer: \nLocation: \nPosition Type: \nCore Competency: \n", job1.toString());
+        Job job3 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType(""), new CoreCompetency(""));
+        assertEquals("\nID: " + job3.getId() + "\nName: Product Tester\nEmployer: ACME\nLocation: Desert\nPosition Type: Data not available\nCore Competency: Data Not available\n", job3.toString());
 
     }
 }
